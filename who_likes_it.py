b1834124060a1ad3dbc82c06526b0ea4eb5e5fd0
matthewlee1 <hyeonjin.lee@uconn.edu>
2020-09-26 20:20:51 -0400

    add Who likes it problem

---
+++ who_likes_it.py
@@ -0,0 +1,13 @@
+def likes(names):
+    #your code here
+    d = {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }
+    length = len(names)
+    return d[min(4,length)].format(*names,others = length - 2)
+
+            
